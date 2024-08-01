a6a2712ef58e407dfe74b06f06eec7815f26d3b8
rvlab <qhgus9346@gmail.com>
2024-08-01 19:02:30 +0900

    update

--- sbb/src/main/java/com/example/demo/answer/AnswerController.java
+++ sbb/src/main/java/com/example/demo/answer/AnswerController.java
@@ -7,8 +7,10 @@ import com.example.demo.question.QuestionService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable; //id 값을 얻을 때
 import org.springframework.web.bind.annotation.RequestMapping; //url 프리픽스
@@ -22,9 +24,13 @@ public class AnswerController {
 	private final AnswerService answerService;
 	
 	@PostMapping("/create/{id}")
-	public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+	public String createAnswer(Model model, @PathVariable("id") Integer id, @Valid AnswerForm answerForm, BindingResult bindingResult) {
 		Question question = this.questionService.getQuestion(id);
-		this.answerService.create(question, content);
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("question", question);
+			return "question_detail";
+		}
+		this.answerService.create(question, answerForm.getContent());
 		return String.format("redirect:/question/detail/%s", id);
 	}
 }
--- sbb/src/main/java/com/example/demo/question/QuestionController.java
+++ sbb/src/main/java/com/example/demo/question/QuestionController.java
@@ -3,11 +3,17 @@ package com.example.demo.question;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable; //id 값을 얻을 때
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.answer.AnswerForm;
 
 @RequestMapping("/question") //url 프리픽스
 @RequiredArgsConstructor
@@ -24,10 +30,25 @@ public class QuestionController {
 	}
 	
 	@GetMapping(value = "/detail/{id}")//url 프리픽스 설정으로 앞에 /question 없어도 됨
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
 		}
+	
+	@GetMapping("/create")
+	public String questionCreate(QuestionForm questionForm) {
+		return "question_form";
+	}
+	
+	//매개변수의 형태가 다르면 메서드명이 같아도 됨 오버로딩
+	@PostMapping("/create")
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+		return "redirect:/question/list";
+	}
 	
 }
